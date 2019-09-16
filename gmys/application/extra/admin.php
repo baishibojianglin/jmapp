d8ca948b7c81fdf46cc67a9e54c8fff3390717f9
Greetme <1980900198@qq.com>
2019-09-16 20:27:52 +0800

    test2

--- gmys/application/extra/admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    // session设置
-    'session_admin' => 'gmys_admin', // 管理员session名称
-    'session_admin_scope' => 'gmys_admin_scope', // 作用域
-];
--- gmys/application/extra/app.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'password_pre_salt' => '#gmys', // 密码加密盐
-
-    'SERVER_NAME' => 'http://' . $_SERVER['SERVER_NAME'] . '/index.php/', // 当前域名
-];
