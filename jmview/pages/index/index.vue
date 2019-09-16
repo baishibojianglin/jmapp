35a78d633ca9a586cb0dbb0b9114286cd0ee823e
jl <904880305@qq.com>
2019-09-16 17:58:32 +0800

    create pro

---
+++ jmview/pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="content">
+		<view class="imglist">
+			<image class="img" src="/static/images/xd1.jpg"></image>
+		</view>
+		<view class="imglist">
+           <text>{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: ''
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		width: 100%;
+		margin: 0;
+		padding: 0;
+	}
+	.imglist {
+		width: 100%;
+		margin: 0;
+		padding: 0;
+	}
+	.img {
+		width: 100%;
+	}
+
+
+
+</style>
