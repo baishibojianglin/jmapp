537e1b01d9c4f2e0d0e113acae10200fcde0c3b4
jl <904880305@qq.com>
2019-09-19 16:37:30 +0800

    change index

--- jmview/pages/index/index.vue
+++ jmview/pages/index/index.vue
@@ -1,33 +1,17 @@
 <template>
 	<view>
-		<vrow class="content">
-		    <vcol span="100"><image class="img" v-bind:src="thumb"></image></vcol>
+		<vrow class="content" v-for="item in artitle_list">
+		    <vcol span="100">
+			    <navigator>
+				  <image class="img" v-bind:src="item.thumb"></image>
+				</navigator>
+		    </vcol>				  
 		    <vcol span="70">
-		        <vrow retract="1">{{title}}</vrow>
-				<vrow class="subtitle" retract="1">{{house_type}} | {{area}} | {{price}}万</vrow>
+		        <vrow retract="1">{{item.title}}</vrow>
+				<vrow class="subtitle" retract="1">{{item.house_type}} | {{item.area}} | {{item.price}}万</vrow>
 		    </vcol>
 			<vcol span="30">
-			    <vrow retract="1"><button class="button">预约设计</button></vrow>
-			</vcol>
-		</vrow>
-		<vrow class="content">
-		    <vcol span="100"><image class="img" v-bind:src="thumb"></image></vcol>
-		    <vcol span="70">
-		        <vrow retract="1">{{title}}</vrow>
-				<vrow class="subtitle" retract="1">{{house_type}} | {{area}} | {{price}}万</vrow>
-		    </vcol>
-			<vcol span="30">
-			    <vrow retract="1"><button class="button">预约设计</button></vrow>
-			</vcol>
-		</vrow>
-		<vrow class="content">
-		    <vcol span="100"><image class="img" v-bind:src="thumb"></image></vcol>
-		    <vcol span="70">
-		        <vrow retract="1">{{title}}</vrow>
-				<vrow class="subtitle" retract="1">{{house_type}} | {{area}} | {{price}}万</vrow>
-		    </vcol>
-			<vcol span="30">
-			    <vrow retract="1"><button class="button">预约设计</button></vrow>
+			    <vrow retract="1"><button class="button" @click="callphone(item.phone)">预约设计</button></vrow>
 			</vcol>
 		</vrow>
 	</view>
@@ -43,19 +27,27 @@
 		},
 		data() {
 			return {
-				title: '保利梧桐语 90平',
-				thumb:'/static/images/xd1.jpg',
-				introduce:'现代简约',
-				house_type:'3室2厅2卫',
-				area:'70-90平',
-				price:15			
+		      artitle_list:[]
 			}
 		},
 		onLoad() {
-
+			    //默认加载文章列表
+			     var self=this;
+				 uni.request({
+					url:'/dpc/index/Index/index',
+					method:'GET',
+					success:function(res){
+						self.artitle_list=res.data;
+					}
+				 })         
 		},
 		methods: {
-
+               //拨打客户电话
+			   callphone(phone){
+				   uni.makePhoneCall({
+				       phoneNumber: phone 
+				   });
+			   }
 		}
 	}
 </script>
@@ -74,14 +66,11 @@
 	}
 	.button{
 		background:-webkit-linear-gradient(left,#695648,#82858A); /* Safari 5.1 - 6.0 */
-		background:linear-gradient(left,#695648,#82858A);
+		background:linear-gradient(left,#695648,#82858A);/*chrom*/
 		background:-ms-linear-gradient(left,#695648,#82858A);/* IE 10 */
 		background:-moz-linear-gradient(left,#695648,#82858A);/*火狐*/ 
 		background:-o-linear-gradient(left,#695648,#82858A); /* Opera 11.1 - 12.0 */
 		color:#fff;
 		margin-top: 5upx;
 	}
-
-
-
 </style>
