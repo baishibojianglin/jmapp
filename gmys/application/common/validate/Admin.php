bbdf232f1d4f5c37f98598480f61c248a7e42393
Greetme <1980900198@qq.com>
2019-09-23 16:32:45 +0800

    add function updateAdminPassword() in gmys/app/admin/

--- gmys/application/common/validate/Admin.php
+++ gmys/application/common/validate/Admin.php
@@ -8,7 +8,7 @@ class Admin extends Validate
 {
     protected $rule = [
         'admin_name' => 'require|unique:admin|max:20',
-        'password' => 'require|max:20',
+        'password' => 'require|confirm|max:20',
     ];
 
     protected $message = [
@@ -17,10 +17,12 @@ class Admin extends Validate
         'admin_name.max'     => '管理员名称长度不能超过20',
         'password.require' => '密码不能为空',
         'password.max'     => '密码长度不能超过20',
+        'password.confirm' => '密码和确认密码不一致',
     ];
 
     protected $scene = [
-        'login' => ['admin_name' => 'require|max:20', 'admin_name.unique' => 'unique:admin, admin_name^admin_id', 'password'], // 忽略唯一(unique)类型字段admin_name对自身数据的唯一性验证
+        'login' => ['admin_name' => 'require|max:20', 'admin_name.unique' => 'unique:admin, admin_name^admin_id', 'password' => 'require|max:20'], // 忽略唯一(unique)类型字段admin_name对自身数据的唯一性验证
         'update' => ['admin_name' => 'require|max:20', 'admin_name.unique' => 'unique:admin, admin_name^admin_id'],
+        'update_password' => ['password'], // 更新密码场景
     ];
 }
