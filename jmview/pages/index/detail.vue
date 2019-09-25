afcd279ff48f9fa04793a080aa3572dfe3b09717
jl <904880305@qq.com>
2019-09-25 18:12:42 +0800

    change detail

--- jmview/pages/index/detail.vue
+++ jmview/pages/index/detail.vue
@@ -1,13 +1,23 @@
 <template>
-	<view>
-		<vrow>
-		    <vcol span="30">
-               <vrow class="content">
-				   <image class="img" :src="article_content.avatar"></image>
+	<view class="content">
+		<vrow class="borbottom">
+		    <vcol span="35" class="center fon25">
+               <vrow>
+				   <image class="img" :src="content.avatar"></image>
 			   </vrow>
+			   <vrow class="designname">设计师：{{content.member_name}}</vrow>
 		    </vcol>	
-		    <vcol span="60">
-				
+		    <vcol span="65" class="center fon25 colheight">
+                <vrow class="colheight-vrow">
+					<vcol span="21">风格</vcol>
+					<vcol span="21">大小</vcol>
+					<vcol span="21">造价</vcol>
+				</vrow>
+				<vrow>
+					<vcol span="21">{{content.keywords}}</vcol>
+					<vcol span="21">{{content.area}}</vcol>
+					<vcol span="21">{{content.price}}</vcol>
+				</vrow>
 		    </vcol>	
 		</vrow>
 	</view>
@@ -16,14 +26,15 @@
 <script>
 	import vrow from '@/components/lml-layout/row.vue'
 	import vcol from '@/components/lml-layout/col.vue'
-	
+
+		
 	export default {
 		components:{
 			vrow,vcol
 		},
 		data() {
 			return {
-		      article_content:{}
+		      content:{}
 			}
 		},
 		onLoad(options) {
@@ -35,7 +46,7 @@
 	        	method:'POST',
 				header:{'content-type':'application/x-www-form-urlencoded'},
 	        	success:function(res){
-	        		self.article_content=res.data;
+	        		self.content=res.data;
 	        	}
 	         }) 
 		},
@@ -52,10 +63,43 @@
 		align-items: center;
 		justify-content: center;
 	}
+	.borbottom{
+		background:-webkit-linear-gradient(left,#82858A,#695648); /* Safari 5.1 - 6.0 */
+		background:linear-gradient(left,#82858A,#695648);/*chrom*/
+		background:-ms-linear-gradient(left,#82858A,#695648);/* IE 10 */
+		background:-moz-linear-gradient(left,#82858A,#695648);/*火狐*/ 
+		background:-o-linear-gradient(left,#82858A,#695648); /* Opera 11.1 - 12.0 */
+	}
+	.center {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+	}
+	.fon25{
+		font-size: 25upx;
+		color:#fff;
+	}
 	.img {
-		width:150upx;
-		height:150upx;
+		width:140upx;
+		height:140upx;
 		border-radius: 100%;
 		margin: 15upx;
+	}
+	.mp0{
+		margin: 0;
+		padding: 0;
+	}
+	.colheight{
+		line-height: 100upx;
+		color:#fff;
+		border-left:1px solid #E3E0D5;
+	}
+	.colheight-vrow{
+		border-bottom: 1px solid #E3E0D5;
+	}
+	.designname{
+		padding-bottom: 10upx;
 	}
 </style>
