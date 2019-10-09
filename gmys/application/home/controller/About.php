3982516ea41554aa0905e2277ab5e69057b6ed69
Greetme <1980900198@qq.com>
2019-10-09 17:53:38 +0800

    add team member in gmys/home/about

--- gmys/application/home/controller/About.php
+++ gmys/application/home/controller/About.php
@@ -14,6 +14,10 @@ class About extends Base
      */
     public function about()
     {
-        return view();
+        // 获取团队成员列表
+        $memberList = model('Member')->where(['status' => config('code.status_enable'), 'is_delete' => config('code.not_delete')])->limit(6)->select();
+        $memberList = json_decode(json_encode($memberList), true);
+
+        return view('', ['memberList' => $memberList]);
     }
 }
